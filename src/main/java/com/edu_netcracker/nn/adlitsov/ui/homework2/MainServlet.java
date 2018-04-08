dde91e07f80d11f46ed1e6a2de848a09de04f6f6
alex <adlitsov@gmail.com>
2018-04-08 18:14:29 +0300

    MainServlet.java: now responses with 400 if input is invalid; other mapping error just logged without any actions

--- src/main/java/com/edu_netcracker/nn/adlitsov/ui/homework2/MainServlet.java
+++ src/main/java/com/edu_netcracker/nn/adlitsov/ui/homework2/MainServlet.java
@@ -1,21 +1,29 @@
 package com.edu_netcracker.nn.adlitsov.ui.homework2;
 
-import javax.servlet.ServletException;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class MainServlet extends HttpServlet {
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        String coeffs = request.getParameter("coeffs");
-        QuadraticEquation equation = mapper.readValue(coeffs, QuadraticEquation.class);
-        Solution solution = equation.getSolution();
-        response.setContentType("application/json");
-        response.setHeader("Cache-Control", "no-cache");
-        response.getWriter().write(mapper.writeValueAsString(solution));
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            String coeffs = request.getParameter("coeffs");
+            QuadraticEquation equation = mapper.readValue(coeffs, QuadraticEquation.class);
+            Solution solution = equation.getSolution();
+            response.setContentType("application/json");
+            response.setHeader("Cache-Control", "no-cache");
+            response.getWriter().write(mapper.writeValueAsString(solution));
+        } catch (JsonParseException e) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        }
     }
 }
