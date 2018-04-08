407f1eeadab87241dca4e65e85548929c997eedc
alex <adlitsov@gmail.com>
2018-04-08 18:13:40 +0300

    QuadraticEquation.java, Solution.java: switched back for single HAS_ROOTS case; removed main method

--- src/main/java/com/edu_netcracker/nn/adlitsov/ui/homework2/QuadraticEquation.java
+++ src/main/java/com/edu_netcracker/nn/adlitsov/ui/homework2/QuadraticEquation.java
@@ -3,10 +3,6 @@ package com.edu_netcracker.nn.adlitsov.ui.homework2;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.IOException;
 
 import static com.edu_netcracker.nn.adlitsov.ui.homework2.Solution.Status;
 
@@ -34,30 +30,26 @@ public class QuadraticEquation {
 
     @JsonIgnore
     public Solution getSolution() {
-        if (a == 0 && b == 0 && c == 0) {
-            return new Solution(Status.ALL_REAL_NUMBERS, null);
+        if (a == 0 && b == 0) {
+            if (c == 0) {
+                return new Solution(Status.ALL_REAL_NUMBERS, null);
+            } else {
+                return new Solution(Status.NO_ROOTS_IN_REAL_NUMBERS, null);
+            }
         }
         double discr = Math.pow(b, 2) - 4 * a * c;
         if (discr < 0) {
             return new Solution(Status.NO_ROOTS_IN_REAL_NUMBERS, null);
         } else {
+            Status status = Status.HAS_ROOTS;
             if (discr == 0) {
-                return new Solution(Status.SINGLE_ROOT, new double[]{-b / (2 * a)});
+                return new Solution(status, new double[]{-b / (2 * a)});
             } else {
-                return new Solution(Status.TWO_ROOTS, new double[]{
+                return new Solution(status, new double[]{
                         (-b + Math.sqrt(discr)) / (2 * a),
                         (-b - Math.sqrt(discr)) / (2 * a)
                 });
             }
         }
-    }
-
-    public static void main(String[] args) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-
-        String equationStr = "{\"a\": 1, \"b\": 2, \"c\": 3}";
-        QuadraticEquation eq = mapper.readValue(equationStr, QuadraticEquation.class);
-        String rootsStr = mapper.writeValueAsString(eq);
-        System.out.println(rootsStr);
     }
 }
--- src/main/java/com/edu_netcracker/nn/adlitsov/ui/homework2/Solution.java
+++ src/main/java/com/edu_netcracker/nn/adlitsov/ui/homework2/Solution.java
@@ -1,16 +1,11 @@
 package com.edu_netcracker.nn.adlitsov.ui.homework2;
 
-import java.io.IOException;
-import java.util.Arrays;
-
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Solution {
     public enum Status {
-        ALL_REAL_NUMBERS, NO_ROOTS_IN_REAL_NUMBERS, SINGLE_ROOT, TWO_ROOTS
+        ALL_REAL_NUMBERS, NO_ROOTS_IN_REAL_NUMBERS, HAS_ROOTS
     }
 
     private Status status;
@@ -28,17 +23,5 @@ public class Solution {
 
     public double[] getRoots() {
         return roots;
-    }
-
-    public static void main(String[] args) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-
-        Solution sol = new Solution(Status.ALL_REAL_NUMBERS, new double[]{});
-        String solStr = mapper.writeValueAsString(sol);
-        System.out.println(solStr);
-
-        sol = mapper.readValue(solStr, Solution.class);
-
-        new Solution(Status.ALL_REAL_NUMBERS, new double[]{1});
     }
 }
