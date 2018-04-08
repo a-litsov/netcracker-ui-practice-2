63adeb1cbc8d1e5a7ee65d7c42bab9e1258fbe9a
alex <adlitsov@gmail.com>
2018-04-08 18:58:55 +0300

    frontend: cleaned js code, minor changes in design and page structure

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -1,14 +1,20 @@
-var allNumbersTitle = "True for all real numbers";
-var noRootsTitle = "There are no solutions in real numbers";
-var precision = 2;
+const HAS_ROOTS_CASE = "HAS_ROOTS";
+const SPECIAL_ROOTS_CASES = Object.freeze({
+    "ALL_REAL_NUMBERS": "True for all real numbers",
+    "NO_ROOTS_IN_REAL_NUMBERS": "There are no solutions in real numbers"
+});
+const PRECISIONS = Object.freeze({
+    "coeff": 2,
+    "root": 4
+});
 
-// call it also before ajax query!
 function checkInput() {
     var isValid = true;
-    forEachCoefField(function(field) {
+    forEachCoefField(function (field) {
         isValid = isValid && field.validity.valid;
     });
     document.getElementById("find-btn").disabled = !isValid;
+    return isValid;
 }
 
 function showRootsPanel(rootsPanel) {
@@ -27,7 +33,7 @@ function hideHistTableDiv() {
 
 function removeRootLabels(rootsPanel) {
     var rootLabels = rootsPanel.getElementsByClassName("root");
-    Array.from(rootLabels).forEach(function(r) {
+    Array.from(rootLabels).forEach(function (r) {
         rootsPanel.removeChild(r);
     });
 }
@@ -42,16 +48,12 @@ function addRootLabel(rootsPanel, root) {
 function remakeRootsPanel(rootsPanel, solution) {
     removeRootLabels(rootsPanel);
     var status = solution.status;
-    if (status === "ALL_REAL_NUMBERS") {
-        addRootLabel(rootsPanel, allNumbersTitle);
-    } else {
-        if (status === "NO_ROOTS_IN_REAL_NUMBERS") {
-            addRootLabel(rootsPanel, noRootsTitle);
-        } else {
-            for (i = 0; i < solution.roots.length; i++) {
-                addRootLabel(rootsPanel, solution.roots[i]);
-            }
+    if (status === HAS_ROOTS_CASE) {
+        for (i = 0; i < solution.roots.length; i++) {
+            addRootLabel(rootsPanel, solution.roots[i]);
         }
+    } else {
+        addRootLabel(rootsPanel, SPECIAL_ROOTS_CASES[status]);
     }
 }
 
@@ -62,34 +64,31 @@ function removeHistTableRow(row) {
 
 function addHistTableRow(table, solution) {
     var row = table.insertRow(1);
+    var columnsCount = table.rows[0].cells.length;
+    var coeffsCount = 0;
     var cell;
-    forEachCoefField(function(field) {
+
+    forEachCoefField(function (field) {
         cell = row.insertCell(-1);
-        cell.innerHTML = parseFloat(field.value).toFixed(precision);
+        cell.innerHTML = parseFloat(field.value).toFixed(PRECISIONS["coeff"]);
+        coeffsCount++;
     });
 
-    if (solution.status !== "TWO_ROOTS") {
-        cell = row.insertCell(-1);
-        cell.colSpan = 2;
-        switch (solution.status) {
-            case "ALL_REAL_NUMBERS":
-                cell.innerHTML = "True for all real numbers";
-                break;
-            case "NO_ROOTS_IN_REAL_NUMBERS":
-                cell.innerHTML = "There are no solutions in real numbers";
-                break;
-            case "SINGLE_ROOT":
-                cell.innerHTML = solution.roots[0].toPrecision(precision);
-                break;
+    if (solution.status === HAS_ROOTS_CASE) {
+        var colSpan = (columnsCount - coeffsCount) / solution.roots.length;
+        for (var i = 0; i < solution.roots.length; i++) {
+            cell = row.insertCell(-1);
+            cell.innerHTML = solution.roots[i].toFixed(PRECISIONS["root"]);
+            cell.colSpan = (i === 0) ? Math.ceil(colSpan) : Math.floor(colSpan);
         }
     } else {
-        for (i = 0; i < 2; i++) {
-            cell = row.insertCell(-1);
-            cell.innerHTML = solution.roots[i].toPrecision(precision);
-        }
+        cell = row.insertCell(-1);
+        cell.colSpan = columnsCount - coeffsCount;
+        cell.innerHTML = SPECIAL_ROOTS_CASES[solution.status];
+        cell.style.textAlign = "center";
     }
 
-    row.onclick = function() {
+    row.onclick = function () {
         if (table.rows.length === 2) {
             hideHistTableDiv();
         }
@@ -97,30 +96,33 @@ function addHistTableRow(table, solution) {
     };
 }
 
-function getCoeffs() {
+function getCoeffsAsObj() {
     var coeffs = {};
-    forEachCoefField(function(field) {
-        coeffs[field.id.charAt(0)] = parseFloat(field.value).toFixed(precision);
+    forEachCoefField(function (field) {
+        coeffs[field.id.charAt(0)] = parseFloat(field.value);
     });
     return coeffs;
 }
 
 function forEachCoefField(action) {
-    var fields = document.getElementsByClassName("coef");
+    var fields = document.getElementsByClassName("coeff");
     Array.from(fields).forEach(action);
 }
 
 
 function findRoots() {
+    if (!checkInput()) {
+        return;
+    }
     var historyTable = document.getElementById("history-table");
     var rootsPanel = document.getElementById("current-roots-panel");
 
     var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
     var url = window.location.href + "/quadratic-equations-solver";
     xhr.open('POST', url);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState>3 && xhr.status === 200) {
-            var solution =  JSON.parse(xhr.responseText);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState > 3 && xhr.status === 200) {
+            var solution = JSON.parse(xhr.responseText);
 
             remakeRootsPanel(rootsPanel, solution);
             showRootsPanel(rootsPanel);
@@ -130,5 +132,5 @@ function findRoots() {
         }
     };
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.send("coeffs=" + JSON.stringify(getCoeffs()));
+    xhr.send("coeffs=" + JSON.stringify(getCoeffsAsObj()));
 }
